e0d11f431771ace9c507deda4b4f1c7502e5b5de
jinyi <jinyiwei2012@philia093.ren>
2026-05-25 21:38:10 +0800

    fix: hf_loader transformers import捕捉Exception而非仅ImportError

--- algorithms/training/hf_loader.py
+++ algorithms/training/hf_loader.py
@@ -28,7 +28,7 @@ _hf_available = True
 try:
     import transformers  # noqa: F401
     import huggingface_hub  # noqa: F401
-except ImportError:
+except Exception:
     _hf_available = False
 
 # Repo ID 常量
