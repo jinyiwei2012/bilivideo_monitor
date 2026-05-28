1771f849722e9bffa63cd20e1d798d9ca147e552
jinyi <jinyiwei2012@philia093.ren>
2026-05-28 20:24:32 +0800

    chore: 增加 TheSpeedX SOCKS4/SOCKS5 代理源

--- core/proxy_manager.py
+++ core/proxy_manager.py
@@ -381,6 +381,8 @@ class ProxyManager:
     PROXY_SOURCES = [
         "https://proxylist.geonode.com/api/proxy-list?limit=30&page=1&sort_by=lastChecked&sort_type=desc&protocols=http%2Chttps%2Csocks4%2Csocks5",
         "https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt",
+        "https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks4.txt",
+        "https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks5.txt",
         "https://raw.githubusercontent.com/hookzof/socks5_list/master/proxy.txt",
     ]
 
